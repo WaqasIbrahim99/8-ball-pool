76079f4a8ed214531fa974a64f09324d5d289287
SF <shaikhwaqasibrahim@gmail.com>
2021-10-01 02:18:33 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+
+    home: Scaffold(
+      backgroundColor: Colors.teal,
+      appBar: AppBar(
+        title: Text('My Magic ball App'),
+        backgroundColor: Colors.blue,
+      ),
+      body: magic8ball(),
+    ),
+  ));
+}
+class magic8ball extends StatefulWidget {
+
+  @override
+  _magic8ballState createState() => _magic8ballState();
+}
+
+class _magic8ballState extends State<magic8ball> {
+  int ballNumber=1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisAlignment: MainAxisAlignment.center,
+      children: [Center(child: FlatButton(onPressed: () {
+        setState(() {
+         
+        });
+      }, child: Image.asset('images/ball$ballNumber.png'),),),
+      ],);
+  }
+}
